5e8e1a1ce1f7c6ed3b1bc9a591d66a44480be195
Michael Taylor <247michaeltaylor@gmail.com>
2025-11-03 11:35:27 -0600

    more codebase additions

---
+++ server/app.js
@@ -0,0 +1,16 @@
+
+// Default Express.js file example
+
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
