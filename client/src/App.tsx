d314fb9f9c77b4f2cfabadb06f8ccd70f9e0fae1
Alexxander Rubio <alexxrubio@tamu.edu>
2025-11-08 17:51:52 -0600

    Hell yeah

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Home from "./pages/any/Home.tsx";
-import Login from "./pages/any/Login.tsx";
-import Signup from "./pages/any/Signup.tsx";
-import CashierHome from "./pages/cashier/CashierHome.tsx";
-import ManagerHome from "./pages/manager/ManagerHome.tsx";
-import CustomerHome from "./pages/customer/CustomerHome.tsx";
+import Home from './pages/any/Home.tsx';
+import Login from './pages/any/Login.tsx';
+import Signup from './pages/any/Signup.tsx';
+import CashierHome from './pages/cashier/CashierHome.tsx';
+import ManagerHome from './pages/manager/ManagerHome.tsx';
+import CustomerHome from './pages/customer/CustomerHome.tsx';
 
 function App() {
     return (
