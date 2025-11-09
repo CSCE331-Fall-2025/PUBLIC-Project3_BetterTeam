055305280293ee88849bef93f3bfe87bd5dee48b
Ethan-Dom <EthanDominguez77573@tamu.edu>
2025-11-08 18:31:37 -0600

    menu shell

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/any/Home.tsx';
 import Login from './pages/any/Login.tsx';
 import Signup from './pages/any/Signup.tsx';
+import MenuBoard from './pages/any/MenuBoard.tsx';
 import CashierHome from './pages/cashier/CashierHome.tsx';
 import ManagerHome from './pages/manager/ManagerHome.tsx';
 import CustomerHome from './pages/customer/CustomerHome.tsx';
@@ -15,6 +16,7 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<Signup />} />
+                <Route path="/menu" element={<MenuBoard />} />
                 <Route path="/cashier" element={<CashierHome />} />
                 <Route path="/manager" element={<ManagerHome />} />
                 <Route path="/customer" element={<CustomerHome />} />
--- client/src/pages/any/MenuBoard.tsx
+++ client/src/pages/any/MenuBoard.tsx
@@ -1,14 +1,51 @@
-import { React } from 'react'
+import React from 'react'
 import GuestHeader from '../../components/GuestHeaderComponents/GuestHeader.tsx'
+import { DishBox } from '../../components/DishComponents/DishBox.tsx'
+import './MenuBoard.css'
 
 const page = {
     name: 'Menu-Board',
     user: 'Any',
 };
 
+export interface Dish{
+    name: string;
+    price: number;
+    imageUrl?: string;
+}
+
+const allEntrees: Dish[] = [
+  { name: "Orange Chicken", price: 7, imageUrl: "../../../assets/orangechick.PNG" },
+  { name: "Beijing Beef", price: 7, imageUrl: "../../../assets/beijing.PNG" },
+  { name: "Honey Walnut Shrimp", price: 8, imageUrl: "../../../assets/shrimp.PNG" },
+  { name: "Broccoli Beef", price: 6, imageUrl: "../../../assets/brocbeef.PNG" },
+  { name: "Kung Pao Chicken", price: 7, imageUrl: "../../../assets/kungpao.PNG" }
+];
+
+const allSides: Dish[] = [
+  { name: "Fried Rice", price: 4, imageUrl: "../../../assets/ricefried.PNG" },
+  { name: "Chow Mein", price: 4, imageUrl: "../../../assets/chowmein.PNG" },
+];
+
+const allDrinks: Dish[] = [
+  { name: "Coke", price: 4, imageUrl: "../../../assets/coke.PNG" },
+  { name: "Dr. Pepper", price: 3, imageUrl: "../../../assets/drp.PNG" },
+];
+
+const allApps: Dish[] = [
+  { name: "Cream Cheese Rangoon", price: 3, imageUrl: "../../../assets/rangoon.PNG" },
+  { name: "Veggie Spring Roll", price: 3, imageUrl: "../../../assets/veggieroll.PNG" },
+];
+
 function MenuBoard() {
     return(
-        <GuestHeader name={page.name}/>
+        <>
+            <GuestHeader name={page.name}/>
+            <DishBox title={'Entrees'} dishes = {allEntrees}/>  
+            <DishBox title={'Sides'} dishes = {allSides}/>
+            <DishBox title={'Drinks'} dishes = {allDrinks}/>
+            <DishBox title={'Apps'} dishes = {allApps}/>
+        </>
     );
 }
 
