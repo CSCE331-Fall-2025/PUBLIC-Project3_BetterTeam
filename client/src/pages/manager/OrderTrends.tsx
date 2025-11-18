c9ef73e6ba6e793b8eb63bda3f971105dffb26ee
Ethan-Dom <EthanDominguez77573@tamu.edu>
2025-11-18 12:48:33 -0600

    Split manager page, added them to navbar

--- client/src/pages/manager/OrderTrends.tsx
+++ client/src/pages/manager/OrderTrends.tsx
@@ -1,5 +1,71 @@
+// import { useState } from 'react'
+// import Button from '../../components/ButtonComponents/Button.tsx'
+import Table, { type ColumnDefinition } from '../../components/TableComponents/Table.tsx'
+import 'chart.js/auto'
+import { Line } from 'react-chartjs-2'
+import './OrderTrends.css'
+
+const dishData = {
+    labels: ['Orange Chicken','Fried Rice','Seasonal'],
+    datasets: [
+        {
+            label: 'Dish',
+            data: [100,200,300],
+            borderColor: 'rgba(75, 75, 75, 1)',
+            backgroundColor: 'rgba(0,0,0,0.5)',
+            borderWidth: 2,
+        },
+        {
+            label: 'Dish 2',
+            data: [50,250,75],
+            borderColor: 'rgba(125, 150, 75, 1)',
+            backgroundColor: 'rgba(255,25,150,0.5)',
+            borderWidth: 2,
+        },
+    ],
+}
+
+interface Dish {
+    id: number,
+    dishName: string,
+    category: string,
+}
+
 function OrderTrends() {
-	return;
+
+	function handleClickTable() {
+        alert('WHAT!!! How. How did you know to click me.');
+    }
+
+	const dishTableData: Dish[] = [
+        { id: 1, dishName: 'Orange Chicken', category: 'Entree'},
+        { id: 2, dishName: 'Fried Rice', category: 'Side' },
+        { id: 3, dishName: 'Seasonal Item', category: 'Seasonal'},
+    ];
+
+    const dishColumns: ColumnDefinition<Dish>[] = [
+        {header: 'Dish Id', accessor: (d) => d.id },
+        {header: 'Dish Name', accessor: (d) => d.dishName },
+        {header: 'Category', accessor: (d) => d.category },
+        {
+            header:'Click Dish',
+            accessor: (dish) => (
+                <button onClick={() => handleClickTable()}>
+                    {dish.dishName}
+                </button>
+            ),
+        },
+    ];
+	return(
+		<div className='orderTrends'>
+			<div className='dishChart'>
+				<Line data={dishData} />
+			</div>
+			<div className='tableContainer'>
+				<Table data={dishTableData} columns={dishColumns}/>
+			</div>
+		</div>
+	);
 }
 
 export default OrderTrends;
