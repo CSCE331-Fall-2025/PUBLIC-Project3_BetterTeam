774a065a59c3926f21592eec0215c7a454986dd6
ahad-maknojiya <ahad.maknojiya@tamu.edu>
2025-11-18 19:53:05 -0600

    added google auth and log out button

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import App from './App.tsx'
+
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <GoogleOAuthProvider clientId="1019984704736-3rm24fu64b448k1a6cv0t8c4njlepg8d.apps.googleusercontent.com">
     <App />
+    </GoogleOAuthProvider>
   </StrictMode>,
 )
