c5fa5b3ed6e6bff2e1e870e070172ba0faa2196c
Ethan-Dom <EthanDominguez77573@tamu.edu>
2025-11-23 14:37:23 -0600

    Dish Buttons

--- server/src/models/managerDishModel.js
+++ server/src/models/managerDishModel.js
@@ -1,9 +1,35 @@
 // Need to import this if this file requires the credentials to DB
 import { pool } from "../db.js";
 
+
+export async function newDish(name, type, price) {
+    const result = await pool.query(
+        'INSERT INTO dish (name, price, type) VALUES ($1, $2, $3) RETURNING *;',
+        [name, price, type]
+    );
+    return result.rows[0];
+}
+
+// Simple return the dish table
 export async function getAllDishes() {
     const result = await pool.query(
-        'SELECT * FROM dish;'
+        'SELECT dish_id, name, type, price FROM dish;'
     );
     return result.rows;
+}
+
+export async function updateDish(id, name, type, price){
+    const result = await pool.query(
+        'UPDATE dish SET name = $1, type = $2, price = $3 WHERE dish_id = $4 RETURNING *;',
+        [name, type, price, id]
+    );
+    return result.rows[0];
+}
+
+export async function deleteDish(id){
+    const result = await pool.query(
+        'DELETE FROM dish WHERE dish_id = $1 RETURNING dish_id;',
+        [id]
+    );
+    return result.rows[0];
 }
--- server/src/routes/managerDishRoute.js
+++ server/src/routes/managerDishRoute.js
@@ -1,12 +1,15 @@
 import { Router } from "express";
 
 // one again need to import any routes you want to use
-import { getDishes } from "../controllers/managerDishController.js";
+import { createDish, getDishes, updateDishController, deleteDishController} from "../controllers/managerDishController.js";
 
 // create an express router
 const router = Router();
 
-// so this route is relative to the the example route in index.html and already has /api/manager/dish in front of it. Now it routes the full path of /api/manager/dish/ to the controller getDishes.
+// so this route is relative to the the example route in index.html and already has /api/manager/dish in front of it. Now it routes the full path of /api/manager/dish/ to the controller getDishes
+router.post("/", createDish);
 router.get("/", getDishes);
+router.put("/:dish_id", updateDishController);
+router.delete("/:dish_id", deleteDishController);
 
 export default router;
