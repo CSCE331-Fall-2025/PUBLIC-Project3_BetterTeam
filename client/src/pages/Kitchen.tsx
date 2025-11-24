67dae435a4c2eda29e7c0e5f0bdcb02877118da2
Alex P <charhp713@gmail.com>
2025-11-23 22:05:10 -0600

    quick commit while I fix something else

--- client/src/pages/Kitchen.tsx
+++ client/src/pages/Kitchen.tsx
@@ -7,49 +7,18 @@ import './Kitchen.css';
 
 type Order = OrderCardProps;
 
-/*interface KitchenProps{
-	//orders: Order[];
-}*/
-
-/*interface OrderCard = {
-	name: string;
-}*/
-const allEntrees: Dish[] = [
+/*const allEntrees: Dish[] = [
   { dish_id: 0, name: "Orange Chicken", price: 7},
   { dish_id: 0, name: "Beijing Beef", price: 7},
   { dish_id: 0, name: "Honey Walnut Shrimp", price: 8},
   { dish_id: 0, name: "Broccoli Beef", price: 6},
   { dish_id: 0, name: "Kung Pao Chicken", price: 7}
-];
-
-/*
-const ordersNotStarted : Order[] = [
-	{name: "t00", slot: 0, items: allEntrees},
-	{name: "t01", slot: 0, items: []},
-	{name: "t02", slot: 0, items: []},
-	{name: "t03", slot: 0, items: []},
-];
-
-const ordersStarted : Order[] = [
-	{name: "t10", slot: 1, items: []},
-	{name: "t11", slot: 1, items: []},
-];
-
-const ordersDone : Order[] = [
-	{name: "t20", slot: 2, items: []},
-	{name: "t21", slot: 2, items: []},
-	{name: "t22", slot: 2, items: []},
 ];*/
+
 const Orders : Order[] = [
-	{name: "t00", slot: 0, items: allEntrees},
+	/*{name: "t00", slot: 0, items: allEntrees},
 	{name: "t01", slot: 1, items: allEntrees},
-	{name: "t02", slot: 0, items: allEntrees},
-	/*{name: "t03", slot: 0, items: []},
-	{name: "t10", slot: 1, items: []},
-	{name: "t11", slot: 1, items: []},
-	{name: "t20", slot: 2, items: []},
-	{name: "t21", slot: 2, items: []},
-	{name: "t22", slot: 2, items: []},*/
+	{name: "t02", slot: 0, items: allEntrees},*/
 ];
 //<OrderBox title={"Completed"} 	slot={2} orders={ordersDone} />
 function Kitchen() {
