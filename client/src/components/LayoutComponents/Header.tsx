0ab81bce1c8eb5f6beba9b473d58ff6d78fe52d9
Michael Taylor <michaeltaylor247@tamu.edu>
2025-12-03 20:17:24 -0600

    styling blows -- Layout (navbar, header, pagecontent) styled ?

--- client/src/components/LayoutComponents/Header.tsx
+++ client/src/components/LayoutComponents/Header.tsx
@@ -6,13 +6,11 @@ import { useAuth } from "../../context/AuthContext";
 export default function Header() {
     const { user } = useAuth();
     return (
-        <>
         <header>
             <h1 id="Greeting"> Hello, {user?.name ?? "Guest"} </h1>
             <h1 id="Time"> TIME </h1>
             <img id="Logo" src="../assets/panda.png" title="Panda Express Logo" ></img>
         </header>
-    </>
   )
 }
 
