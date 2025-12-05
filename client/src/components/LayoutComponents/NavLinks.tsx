8cd2285be85b72f9ad29f2e915a6240cf94a001f
Alexxander Rubio <alexxrubio@tamu.edu>
2025-12-05 14:24:48 -0600

    new home screen

--- client/src/components/LayoutComponents/NavLinks.tsx
+++ client/src/components/LayoutComponents/NavLinks.tsx
@@ -15,6 +15,7 @@ export function GuestLinks() {
     <div className="nav">
         <div className="nav-section">
             <p className="nav-section-title">Guest</p>
+            <NavLink className="nav-item" to="/any/home">Home Screen</NavLink>
             <NavLink className="nav-item" to="/customer/customerhome">Order</NavLink>
             <NavLink className="nav-item" to="/customer/customercheckout"> Cart{count > 0 ? ` ${count}` : ""} </NavLink>
             <NavLink className="nav-item" to="/any/menuboard">Menu</NavLink>
