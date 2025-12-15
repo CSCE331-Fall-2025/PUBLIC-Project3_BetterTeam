563d07e4e23afb26d5bf2fbfb75f4fea7db84d7e
Michael Taylor <michaeltaylor247@tamu.edu>
2025-12-15 00:04:59 -0600

    Totally forgot to propagte changes in OrderCard to OrderBox... it needed to import and use something else

--- client/src/components/KitchenComponents/OrderBox.tsx
+++ client/src/components/KitchenComponents/OrderBox.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { OrderCard } from "./OrderCard";
-import type { OrderCardProps } from "./OrderCard";
+import type { Order } from "./OrderCard";
 import "./OrderBox.css";
 
 interface OrderBoxProps {
     title: string;
     slot: number;
-    orders: OrderCardProps[];
-    onUpdateOrders: (orders: OrderCardProps[]) => void;
+    orders: Order[];
+    onUpdateOrders: (orders: Order[]) => void;
 }
 
 export const OrderBox: React.FC<OrderBoxProps> = ({
@@ -17,16 +17,16 @@ export const OrderBox: React.FC<OrderBoxProps> = ({
 }) => {
     return (
         <div className="order-box">
-        {title && <h2 className="order-box-title">{title}</h2>}
-        <div className="order-box-grid">
-        {orders.map(order => (
-            <OrderCard
-                key={order.name}
-                {...order}
-                onUpdateOrders={onUpdateOrders}
-            />
-        ))}
-        </div>
+            {title && <h2 className="order-box-title">{title}</h2>}
+            <div className="order-box-grid">
+                {orders.map(order => (
+                    <OrderCard
+                        key={order.name}
+                        {...order}
+                        onUpdateOrders={onUpdateOrders}
+                    />
+                ))}
+            </div>
         </div>
     );
 };
